b59bfa44216d8cf89da45a753e572fa2a41198c7
Владимир Мелихов <b1esssed1@yandex.com>
2026-03-03 11:15:38 +0300

    rrr

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Главная страница - свадебное приглашение"""
+    """Главная страница -  свадебное приглашение"""
     return render_template('index.html')
 
 if __name__ == '__main__':
